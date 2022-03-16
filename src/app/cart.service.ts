08b9ca252c06ba657a100d0c6049bd1bfbee24b0
gerald <oforige@gmail.com>
2022-03-16 15:40:59 +0000

    added a checkout view and shipping details form

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,8 @@ import { Product } from './products';
 export class CartService {
 
   items: Product[] = [];
+
+  constructor(private http: HttpClient) { }
 
   addToCart(product: Product){
     this.items.push(product);
@@ -20,5 +23,8 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  constructor() { }
+
+  getShippingPrices(){
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
 }
